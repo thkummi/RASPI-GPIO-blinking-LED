4039abb439a6dd0de5b72a8ff7e360ec6f415c93
thkummi <36160753+thkummi@users.noreply.github.com>
2018-11-24 18:19:22 +0100

    Add files via upload

---
+++ gpio_blink.c
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2018 Thomas Kummer
+licensed under the GNU General Public License v3.0 
+*/
+
+/* Run this BEFORE using this code
+sudo /sys/class/gpio/export
+sudo echo "out" > /sys/class/gpio/gpio4/direction
+sudo chmod 660 /sys/class/gpio/gpio4/value
+*/
+#include <stdio.h>
+#include <unistd.h>
+
+int main () {
+
+
+
+   while(1) {
+        FILE *ptr;
+        ptr = fopen ("/sys/class/gpio/gpio4/value", "w");
+        fprintf(ptr, "1");
+        fclose(ptr);
+        sleep(1);
+        ptr = fopen ("/sys/class/gpio/gpio4/value", "w");
+        fprintf(ptr, "0");
+        fclose(ptr);
+        sleep(1);
+   }
+   /* wird nie erreicht */
+   return 0;
+}
+
